61d3e7d9c0c7ff85e5beed82c369013e1ff2085e
BouniotRaphael <54330390+BouniotRaphael@users.noreply.github.com>
2020-02-23 18:03:25 -1000

    Update verifier_victoire.py

--- verifier_victoire.py
+++ verifier_victoire.py
@@ -1,23 +1,40 @@
-def verifier_victoire_horizontale(grille, numero_joueur, index_ligne, index_colonne):
+def verifier_victoire_verticale(grille, numero_joueur, index_ligne, index_colonne):
     a = 0
-    t = False
+    b = False
     for i in range(len(grille[index_colonne])):
         if grille[index_colonne][i] == numero_joueur:
             a +=1
             if a == 4:
-                t = True
+                b = True
         else:
             a = 0
-    return t
+    return b
 
-def verifier_victoire_vertical(grille, numero_joueur, index_ligne, index_colonne):
+def verifier_victoire_horizontale(grille, numero_joueur, index_ligne, index_colonne):
     a = 0
-    t = False
+    b = False
     for i in range(len(grille[index_ligne])):
         if grille[index_ligne][i] == numero_joueur:
-            a +=1
+            a += 1
             if a == 4:
-                t = True
+                b = True
         else:
             a = 0
-    return t
+    return b
+
+
+def verifier_victoire_diagonale(grille, numero_joueur, index_ligne, index_colonne):
+    a = 0
+    b = False
+    i = index_colonne
+    j = index_ligne
+    while (i <= len(grille[index_colonne]) and j <= len(grille[index_ligne])):
+        if grille[j][i] == numero_joueur:
+            a += 1
+            if a == 4:
+                b = True
+        else:
+            a = 0
+        i = i + 1
+        j = j + 1
+    return b
