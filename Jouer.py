5d75582f94360707f18bab2663883b5651fe0222
BouniotRaphael <54330390+BouniotRaphael@users.noreply.github.com>
2020-02-23 18:24:25 -1000

    Update Jouer.py

--- Jouer.py
+++ Jouer.py
@@ -1,15 +1,15 @@
 def jouer(grille, numero_joueur, index_colonne):
-    if coup_possible(grille, index_colonne) == True:
-        if numero_joueur == 1:
-            for i in range(6):
-                if grille[i][index_colonne] == 0:
-                    grille[i][index_colonne] += 1
-                    break
-        if numero_joueur == 2:
-            for i in range(6):
-                if grille[i][index_colonne] == 0:
-                    grille[i][index_colonne] += 2
-                    break
-    Affiche_grille(grille)
+    if numero_joueur == 1:
+        for i in range(6):
+            if grille[i][index_colonne] == 0:
+                grille[i][index_colonne] == numero_joueur
+                break
+    if numero_joueur == 2:
+        for i in range(6):
+            if grille[i][index_colonne] == 0:
+                grille[i][index_colonne] == numero_joueur
+                break
     return grille
+
+
 
