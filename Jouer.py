4ff7ae5206fd204364bf69a885c9d5f7017228a4
BouniotRaphael <54330390+BouniotRaphael@users.noreply.github.com>
2020-02-24 19:48:17 -1000

    Update Jouer.py

--- Jouer.py
+++ Jouer.py
@@ -1,17 +1,10 @@
 def jouer(grille, numero_joueur, index_colonne):
     """ Fonction permettant de jouer un pion correspondant a "1" lorsque vous êtes le joueur 1 et "2" lorsque vous êtes le joueur 2
     """
-    if numero_joueur == 1:
-        for i in range(6):
+     for i in range(5,-1,-1):
             if grille[i][index_colonne] == 0:
-                grille[i][index_colonne] == numero_joueur
-                break
-    if numero_joueur == 2:
-        for i in range(6):
-            if grille[i][index_colonne] == 0:
-                grille[i][index_colonne] == numero_joueur
-                break
-    return grille
+                grille[i][index_colonne] = numero_joueur
+                return(grille)
 
 
 
