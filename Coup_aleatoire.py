8fe5be001eb34f12d69439d2bf09329dc1f967c3
BouniotRaphael <54330390+BouniotRaphael@users.noreply.github.com>
2020-02-24 19:27:24 -1000

    Update Coup_aleatoire.py

--- Coup_aleatoire.py
+++ Coup_aleatoire.py
@@ -1,4 +1,6 @@
 from random import randint
 def Coup_aleatoire(grille, numero_joueur):
+    """ Fonction permettant de jouer un coup de manière aléatoire
+    """
     aleatoire = randint(0,6)
     return(Jouer(grille,numero_joueur,aleatoire))
