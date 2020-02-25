27180bd274c3c82a33b111dddf247f72d666619d
BouniotRaphael <54330390+BouniotRaphael@users.noreply.github.com>
2020-02-24 19:28:54 -1000

    Update Match_nul.py

--- Match_nul.py
+++ Match_nul.py
@@ -1,4 +1,6 @@
 def Match_nul(grille):
+    """ fonction permettant a Python de determiner si le match est nul ou pas
+    """
     a = False
     for i in range(6):
         if grille[i][index_colonne] != 0:
