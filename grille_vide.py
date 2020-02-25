bf27fcf3d8d31702dc58dc4dc092c45bcc7c24ec
BouniotRaphael <54330390+BouniotRaphael@users.noreply.github.com>
2020-02-24 19:34:48 -1000

    Update grille_vide.py

--- grille_vide.py
+++ grille_vide.py
@@ -1,4 +1,6 @@
 def grille_vide():
+    """ fonction créant la grille dans laquelle le jeu se déroulera
+    """
     a = [0 for i in range(6)],
     grille = [a for j in range(7)]
     return grille   
