d6944b78035dcd1a835173ad535d20c93014a6c6
BouniotRaphael <54330390+BouniotRaphael@users.noreply.github.com>
2020-02-24 19:29:25 -1000

    Update Victoire.py

--- Victoire.py
+++ Victoire.py
@@ -1,4 +1,6 @@
 def Victoire(grille, numero_joueur):
+    """ fonction permettant de determiner si il y a victoire ou pas
+    """
     if Verifier_victoire_horizontale(grille, numero_joueur, index_ligne, index_colonne) or Verifier_victoire_verticale(grille, numero_joueur, index_ligne, index_colonne) or Verifier_victoire_diagonale(grille, numero_joueur, index_ligne, index_colonne) == True:
         return True
     else:
