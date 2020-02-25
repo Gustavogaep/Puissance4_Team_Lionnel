e97b9686fc0952d1cd1ccf2094423b465b7743e3
BouniotRaphael <54330390+BouniotRaphael@users.noreply.github.com>
2020-02-24 19:30:02 -1000

    Update affiche_grille.py

--- affiche_grille.py
+++ affiche_grille.py
@@ -1,4 +1,6 @@
 def Affiche_grille(grille):
+    """ fonction permettant d'afficher la grille où la partie de Puissance 4 se déroulera
+    """
     for suite in grille:
         for caractere in suite:
             if caractere == 1:
