e76f8df3a7ce56b0a9c16c1dad3ad5138b3684af
BouniotRaphael <54330390+BouniotRaphael@users.noreply.github.com>
2020-02-24 19:34:08 -1000

    Update coup_possible.py

--- coup_possible.py
+++ coup_possible.py
@@ -1,4 +1,6 @@
 def Coup_possible(grille, index_colonne):
+    """fonction verifiant si il y a la place pour jouer un coup
+    """
     if grille[0][index_colonne] == 0:
         pas_vide = True
     else:
