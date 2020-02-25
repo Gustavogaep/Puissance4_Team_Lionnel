e7bedd4d2464ab8d72286ea4af06a14f6d6a2a6a
BouniotRaphael <54330390+BouniotRaphael@users.noreply.github.com>
2020-02-24 21:55:50 -1000

    fonction Main

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-
+def Main():
+    """ Fonction principale qui permet de choisir entre 2 modes de jeux:
+    le mode de jeu Ai contre Ai, le mode de jeu Joueur contre Ai et le mode joueur contre joueur.
+    """
+    print("(1)---> Le mode de jeu Ai contre Ai.")
+    print("(2)---> Le mode de jeu Joueur contre Ai.")
+    print("(3)---> Le mode de jeu est Joueur contre Joueur.")
+    try:
+        choix = int(input("Choisissez un mode de jeu:"))
+    except ValueError:
+        print(f"Le choix {choix} est invalide, réessayez.")
+        return None
+    if choix == 1:
+        Ai_vs_ai_aleatoire()
+    elif choix == 2:
+        Joueur_vs_ai_aleatoire()
+    elif choix == 3:
+        joueur_vs_joueur()
+    else:
+        print(f"Le choix {choix} est invalide, réessayez.")
+        Main()
